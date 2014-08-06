3fd3fc4565a845a8ee13068e613802efb0218336
John Chilton <jmchilton@gmail.com>
2014-08-06 17:45:55 -0400

    More progress in grouping repositories.
    No longer attempt to merge groups - maintain one directory per repository - we can go back in later and add symbolic links where needed that will get resolved when uploading tar balls to tool shed.

--- dev2git.py
+++ dev2git.py
@@ -1,5 +1,8 @@
-# TODO: Try to merge suites into a single directory in github.
-# TODO(?): Preserve history.
+#!/usr/bin/env python
+# Usage:
+#   python dev2git.py build
+# Builds a potential mega-repository for Galaxy devteam tools.
+
 import argparse
 import tempfile
 from fabric.api import local
@@ -11,8 +14,11 @@ OWNER = "devteam"
 
 tempdir = tempfile.mkdtemp()
 
-MERGE_REPOS = {
-    "suite_gatk": [
+# These have real homes elsewhere...
+EXCLUDE_REPOS = ["ncbi_blast_plus", "blast_datatypes"]
+
+NEST_REPOS = {
+    "gatk": [
         "analyze_covariates",
         "count_covariates",
         "depth_of_coverage",
@@ -30,7 +36,7 @@ MERGE_REPOS = {
         "variant_select",
         "variants_validate",
     ],
-    "suite_gops": [
+    "gops": [
         "basecoverage",
         "cluster",
         "complement",
@@ -45,7 +51,7 @@ MERGE_REPOS = {
         "subtract_query",
         "tables_arithmetic_operations",
     ],
-    "suite_fastx_toolkit": [
+    "fastx_toolkit": [
         "fasta_clipping_histogram",
         "fasta_formatter",
         "fasta_nucleotide_changer",
@@ -63,8 +69,7 @@ MERGE_REPOS = {
         "fastx_reverse_complement",
         "fastx_trimmer",
     ],
-    "suite_samtools": [
-        "data_manager_sam_fasta_index_builder",
+    "samtools": [
         "bam_to_sam",
         "pileup_interval",
         "sam_to_bam",
@@ -74,18 +79,14 @@ MERGE_REPOS = {
         "samtools_mpileup",
         "samtools_slice_bam",
     ],
-    "suite_hgv": [
-        "hgv_hilbertvis",
-        "snpfreq",
-        "hgv_fundo",
-    ],
-    "suite_cufflinks": [
+    # These packages have old-style "metapackages" predating repository_suite_definitions
+    "cufflinks": [
         "cufflinks",
         "cuffcompare",
         "cuffmerge",
         "cuffdiff",
     ],
-    "suite_vcftools": [
+    "vcftools": [
         "vcftools_annotate",
         "vcftools_compare",
         "vcftools_isec",
@@ -93,6 +94,37 @@ MERGE_REPOS = {
         "vcftools_slice",
         "vcftools_subset",
     ],
+    # These tools don't even have metapackages but I'm trying to control the size of the folder
+    # by grouping them together. Really open to reshuffling this structure.
+    "hgv": [
+        "hgv_hilbertvis",
+        "snpfreq",
+        "hgv_fundo",
+    ],
+    "galaxy_sequence_utils": [
+        "fastq_trimmer",
+        "fastq_to_tabular",
+        "fastq_combiner",
+        "fastq_paired_end_splitter",
+        "fastq_manipulation",
+        "fastqtofasta",
+        "fastq_masker_by_quality",
+        "tabular_to_fastq",
+        "fastq_paired_end_deinterlacer",
+        "fastq_paired_end_interlacer",
+        "fastq_stats",
+        "fastq_groomer",
+        "fastq_filter",
+        "fastq_paired_end_joiner",
+    ],
+    "taxonomy": [
+        "gi2taxonomy",
+        "find_diag_hits",
+        "t2ps",
+        "poisson2test",
+        "lca_wrapper",
+        "t2t_report",
+    ]
 }
 
 SUDO_REPOSITORY_SUITES = [ "all_cufflinks_tool_suite", "all_vcftools" ]
@@ -107,12 +139,18 @@ def build():
     execute("rm -rf repo")
     execute("mkdir repo")
     with lcd("repo"):
-        execute("mkdir tools suites packages datatypes data_managers visualisations") #vis will be empty, needs a gitignore file
+        # vis will be empty, needs a gitignore file
+        execute("mkdir -p tools tool_collections suites packages datatypes data_managers visualisations")
         execute("git init .")
         for repo in dev_repos():
+            repo_name = repo[ "name" ]
             repo_type = repo[ "type" ]
+            if repo_name in EXCLUDE_REPOS:
+                continue
             if repo_type == "tool_dependency_definition":
                 clone_package(repo)
+            elif repo_type == "repository_suite_definition" or repo["name"] in SUDO_REPOSITORY_SUITES:
+                clone_suite(repo)
             elif repo_type == "unrestricted":
                 if repo["name"].find('data_manager') != -1:
                     clone_data_managers(repo)
@@ -120,8 +158,6 @@ def build():
                     clone_datatypes(repo)
                 else:
                     clone_tool(repo)
-            elif repo_type == "repository_suite_definition":
-                clone_suite(repo)
             else:
                 print "WARN: Unhandled repo type %s" % repo_type
 
@@ -130,9 +166,11 @@ def clone_datatypes(repo):
     with lcd("datatypes"):
         clone_repo(repo)
 
+
 def clone_data_managers(repo):
     with lcd("data_managers"):
         clone_repo(repo)
+
 
 def clone_package(repo):
     with lcd("packages"):
@@ -140,8 +178,15 @@ def clone_package(repo):
 
 
 def clone_tool(repo):
-    with lcd("tools"):
-        clone_repo(repo)
+    collection_name = get_repo_collection(repo)
+    if collection_name:
+        with lcd("tool_collections"):
+            execute("mkdir -p %s" % collection_name)
+            with lcd(collection_name):
+                clone_repo(repo)
+    else:
+        with lcd("tools"):
+            clone_repo(repo)
 
 
 def clone_suite(repo):
@@ -173,11 +218,27 @@ def clone_repo(repo):
 
 def destination_directory(repo):
     destination = repo["name"]  # By default just keep in same directory name..
-    for suite, repos in MERGE_REPOS.iteritems():
-        if destination in repos:
-            destination = suite
-            break
+
+    # Was trying to merge the repositories so macro files, wrappers, and
+    # tool-data, etc.. weren't duplicated - but it looks like Dave B. has
+    # done a meticulous job separating out test-data into the correct repo
+    # so I guess it might be best to just keep it one directory per repo here
+    # and go through later and replace things with symbolic links which would
+    # be resolved when uploading tar balls to the tool shed.
+
+    #for suite, repos in NEST_REPOS.iteritems():
+    #    if destination in repos:
+    #        destination = suite
+    #        break
     return destination
+
+
+def get_repo_collection(repo):
+    name = repo["name"]  # By default just keep in same directory name..
+    for collection_name, repos in NEST_REPOS.iteritems():
+        if name in repos:
+            return collection_name
+    return None
 
 
 def summarize_repo(repo):

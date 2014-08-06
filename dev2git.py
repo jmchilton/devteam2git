cf57ffb9da67ed5b42f91f8b4a520c6e0b623e8f
John Chilton <jmchilton@gmail.com>
2014-08-06 15:11:48 -0400

    Fix commit authorship, start work on merging repos.

--- dev2git.py
+++ dev2git.py
@@ -1,6 +1,7 @@
 # TODO: Try to merge suites into a single directory in github.
 # TODO(?): Preserve history.
 import argparse
+import tempfile
 from fabric.api import local
 from fabric.context_managers import lcd
 from bioblend import toolshed
@@ -8,15 +9,51 @@ from bioblend import toolshed
 TOOLSHED = "https://toolshed.g2.bx.psu.edu"
 OWNER = "devteam"
 
+tempdir = tempfile.mkdtemp()
 
+MERGE_REPOS = {
+    "gatk_tools": [
+        "analyze_covariates",
+        "count_covariates",
+        "depth_of_coverage",
+        "indel_realigner",
+        "print_reads",
+        "realigner_target_creator",
+        "table_recalibration",
+        "unified_genotyper",
+        "variant_annotator",
+        "variant_apply_recalibration",
+        "variant_combine",
+        "variant_eval",
+        "variant_filtration",
+        "variant_recalibrator",
+        "variant_select",
+        "variants_validate",
+    ],
+    "gops_tools": [
+        "basecoverage",
+        "cluster",
+        "complement",
+        "concat",
+        "coverage",
+        "flanking_features",
+        "get_flanks",
+        "intersect",
+        "join",
+        "merge",
+        "subtract",
+        "subtract_query",
+        "tables_arithmetic_operations",
+    ]
+}
 
 
 def list():
     repos = dev_repos()
     map( summarize_repo, repos )
 
+
 def build():
-    repos = dev_repos()
     execute("rm -rf repo")
     execute("mkdir repo")
     with lcd("repo"):
@@ -33,6 +70,7 @@ def build():
             else:
                 print "WARN: Unhandled repo type %s" % repo_type
 
+
 def clone_package(repo):
     with lcd("packages"):
         clone_repo(repo)
@@ -40,7 +78,7 @@ def clone_package(repo):
 
 def clone_tool(repo):
     with lcd("tools"):
-        clone_repo(repo)        
+        clone_repo(repo)
 
 
 def clone_suite(repo):
@@ -56,14 +94,28 @@ def dev_repos():
 
 
 def clone_repo(repo):
-    repository_url = "%s/repos/%s/%s %s" % (TOOLSHED, repo["owner"], repo["name"], repo["name"])
-    execute("hg clone %s" % repository_url)
-    with lcd(repo["name"]):
+    repository_url = "%s/repos/%s/%s" % (TOOLSHED, repo["owner"], repo["name"])
+    directory_name = destination_directory(repo)
+    execute("hg clone %s %s/%s" % (repository_url, tempdir, repo["name"]))
+    execute("mkdir -p %s" % directory_name)
+    with lcd(directory_name):
+        execute("cp -r %s/%s/* ." % (tempdir, repo["name"]))
+        execute("rm -rf %s/%s" % (tempdir, repo["name"]))
         execute("rm -rf .hg")
         execute("git add .")
         msg1 = "Initial import of tool shed repository %s" % repo["name"]
         msg2 = "See %s for previous commit history." % repository_url
-        execute('''git commit -m "%s" -m "%s"''' % (msg1, msg2))
+        execute('''git commit --author="devteam <galaxy-lab@bx.psu.edu>" -m "%s" -m "%s"''' % (msg1, msg2))
+
+
+def destination_directory(repo):
+    destination = repo["name"]  # By default just keep in same directory name..
+    for suite, repos in MERGE_REPOS.iteritems():
+        if destination in repos:
+            destination = suite
+            break
+    return destination
+
 
 def summarize_repo(repo):
     print "name: %s, type %s, id %s" % ( repo[ "name" ], repo["type"], repo["id"] )
